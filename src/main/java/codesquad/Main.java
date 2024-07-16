e1f7a92756ddf74d84c0c2792de83ee0623ef9d3
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-16 13:25:56 +0900

    refactor : 리팩토링

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,22 +1,21 @@
 package codesquad;
 
-import codesquad.business.domain.User;
-import codesquad.business.repository.UserRepository;
+import codesquad.business.domain.Member;
+import codesquad.business.service.MemberService;
 import codesquad.was.server.Server;
 import java.io.IOException;
 
-import static codesquad.business.configuration.UrlPathResourceMapConfig.setUrlPathResourceMap;
-import static codesquad.business.configuration.handlerMapConfig.setHandlerMap;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Server server = new Server(10,8080, 10);
-        setUrlPathResourceMap();
-        setHandlerMap();
-        UserRepository userRepository = UserRepository.userRepository;
-        userRepository.save("seungsu",User.factoryMethod("seungsu","승수","123123"));
+
+        Member test = Member.factoryMethod("seungsu", "승수", "123123");
+        Long saved = MemberService.memberService.save(test);
+
+
         try {
             server.run();
         } catch (Exception e) {
