186bc2aa12895f4db7dcd57b15bbb56d05b99fdf
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-16 15:37:15 +0900

    feat : Article Service getAll 메소드 구현

--- src/main/java/codesquad/business/service/ArticleService.java
+++ src/main/java/codesquad/business/service/ArticleService.java
@@ -1,17 +1,26 @@
 package codesquad.business.service;
 
 import codesquad.business.domain.Article;
+import codesquad.business.repository.ArticleJdbcRepository;
 import codesquad.business.repository.ArticleRepository;
+
+import java.util.List;
 
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
-    public ArticleService(ArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
+    public static ArticleService articleService = new ArticleService(ArticleJdbcRepository.articleJdbcRepository);
+
+    public ArticleService(ArticleRepository articleMemoryRepository) {
+        this.articleRepository = articleMemoryRepository;
     }
 
     public long saveArticle(Article article) {
         return articleRepository.save(article);
+    }
+
+    public List<Article> getAllArticles() {
+        return articleRepository.findAll();
     }
 }
