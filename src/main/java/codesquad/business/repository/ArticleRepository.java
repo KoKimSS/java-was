e65626d11cac4fa5ea0ddd88dcae3106bd1067e1
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-16 15:36:47 +0900

    feat : Article Repository interface 구현

--- src/main/java/codesquad/business/repository/ArticleRepository.java
+++ src/main/java/codesquad/business/repository/ArticleRepository.java
@@ -2,31 +2,8 @@ package codesquad.business.repository;
 
 import codesquad.business.domain.Article;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
 
-public class ArticleRepository implements Repository<Long, Article> {
-    private static final ConcurrentHashMap<Long, Article> map = new ConcurrentHashMap<>();
-    private static final AtomicLong idGenerator = new AtomicLong(1); // 초기값 설정
-
-    public static final ArticleRepository ARTICLE_REPOSITORY = new ArticleRepository();
-    private ArticleRepository() {}
-
-    @Override
-    public Article findById(Long key) {
-        return map.get(key);
-    }
-
-    @Override
-    public Long save(Article article) {
-        long andIncrement = idGenerator.getAndIncrement();
-        article.setId(andIncrement);
-        map.put(andIncrement, article);
-        return andIncrement;
-    }
-
-    @Override
-    public void deleteById(Long key) {
-        map.remove(key);
-    }
+public interface ArticleRepository extends Repository<Long, Article> {
+    List<Article> findAll();
 }
