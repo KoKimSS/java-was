ad88950a8289521f311f47282d6f34b266965780
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-19 10:55:19 +0900

    chore :

--- src/main/java/codesquad/business/repository/JdbcTemplate.java
+++ src/main/java/codesquad/business/repository/JdbcTemplate.java
@@ -8,8 +8,8 @@ import static codesquad.was.util.ResourceGetter.getResourceBytesByPath;
 public class JdbcTemplate {
     public static JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
-//    private static final String H2_URL = "jdbc:h2:tcp://localhost/~/java-was";
-    private static final String H2_IN_MEMORY_URL = "jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    private static final String H2_URL = "jdbc:h2:tcp://localhost/~/java-was";
+//    private static final String H2_IN_MEMORY_URL = "jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1";
 private static final String H2_USERNAME = "sa";
     private static final String H2_PASSWORD = "";
 
@@ -29,6 +29,6 @@ private static final String H2_USERNAME = "sa";
 
     public static Connection getConnection() throws SQLException {
 //        return DriverManager.getConnection(MYSQL_URL, MYSQL_USERNAME, MYSQL_PASSWORD);
-        return DriverManager.getConnection(H2_IN_MEMORY_URL, H2_USERNAME, H2_PASSWORD);
+        return DriverManager.getConnection(H2_URL, H2_USERNAME, H2_PASSWORD);
     }
 }
